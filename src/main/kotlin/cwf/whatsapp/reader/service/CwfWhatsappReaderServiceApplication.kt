f815f50ef0c9792dae6202c739f3712ceea4e2d9
MarcoFaccani <m.faccani@alpiantech.com>
2022-10-03 22:16:52 +0200

    first commit

---
+++ src/main/kotlin/cwf/whatsapp/reader/service/CwfWhatsappReaderServiceApplication.kt
@@ -0,0 +1,11 @@
+package cwf.whatsapp.reader.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CwfWhatsappReaderServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CwfWhatsappReaderServiceApplication>(*args)
+}
---
+++ src/test/kotlin/cwf/whatsapp/reader/service/CwfWhatsappReaderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package cwf.whatsapp.reader.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CwfWhatsappReaderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
